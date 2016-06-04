8b14b89c190caf9536e0ab6f14e6c91af460ef95
max <hi@maxlee.im>
2016-06-03 23:39:38 -0400

    update database config

--- manage.py
+++ manage.py
@@ -8,6 +8,13 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
+if os.path.exists('.env'):
+    print('Importing environment from .env...')
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+
 def make_shell_context():
     return dict(app=app, db=db, Blog=Blog, Comment=Comment, Journal=Journal)
 manager.add_command("shell", Shell(make_context=make_shell_context))
