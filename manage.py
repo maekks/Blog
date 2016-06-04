0a6d42088457e7ab76785dbc7943ab9c84bf3374
max <hi@maxlee.im>
2016-06-03 23:45:04 -0400

    update manage.py

--- manage.py
+++ manage.py
@@ -1,4 +1,12 @@
 import os
+
+if os.path.exists('.env'):
+    print('Importing environment from .env...')
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+
 from app import create_app, db
 from app.models import Blog, Comment, Journal, Permission
 from flask_script import Manager, Shell
@@ -7,13 +15,6 @@ from flask_migrate import Migrate, MigrateCommand
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
-
-if os.path.exists('.env'):
-    print('Importing environment from .env...')
-    for line in open('.env'):
-        var = line.strip().split('=')
-        if len(var) == 2:
-            os.environ[var[0]] = var[1]
 
 def make_shell_context():
     return dict(app=app, db=db, Blog=Blog, Comment=Comment, Journal=Journal)
